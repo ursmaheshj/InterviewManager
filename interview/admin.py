8c087bbc71f118bc55d76a06abbd699913950f2c
ursmaheshj <omkar28399@gmail.com>
2024-07-30 22:49:46 +0530

    updated .gitignore

--- interview/admin.py
+++ interview/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from interview.models import Interview
 
 # Register your models here.
-@admin.register(Interview)
-class InterviewAdmin(admin.ModelAdmin):
-    fields = ['company','hr','phone','time','status','comment']
+# @admin.register(Interview)
+# class InterviewAdmin(admin.ModelAdmin):
+#     fields = ['company','hr','phone','time','comment']
+admin.site.register(Interview)
